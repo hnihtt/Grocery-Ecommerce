3dcdb9056eb2cce84df0d6cadece30e6d8afb4bd
VinhThinh <vinhthinhvo@gmail.com>
2026-03-16 16:23:12 +0700

    feat: add mega menu toggle for tablet

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -103,3 +103,26 @@ function handleActiveMenu() {
     });
 }
 
+window.addEventListener("template-loaded", initJsToggle);
+
+function initJsToggle() {
+    $$(".js-toggle").forEach((button) => {
+        const target = button.getAttribute("toggle-target");
+        if (!target) {
+            document.body.innerText = `Cần thêm toggle-target cho: ${button.outerHTML}`;
+        }
+        button.onclick = () => {
+            if (!$(target)) {
+                return (document.body.innerText = `Không tìm thấy phần tử "${target}"`);
+            }
+            const isHidden = $(target).classList.contains("hide");
+
+            requestAnimationFrame(() => {
+                $(target).classList.toggle("hide", !isHidden);
+                $(target).classList.toggle("show", isHidden);
+            });
+        };
+    });
+}
+
+
